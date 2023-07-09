46f3cfc3c5a085046520e11f08ce8b29bdc7dbd8
adriafigueres <adria.figueres@m10c.com>
2023-07-09 17:58:00 +0100

    TwoSum exercice

--- PHP/1.php
+++ PHP/1.php
@@ -13,19 +13,18 @@ class Solution {
      * @return array
      */
     function twoSum($nums, $target) {
-        if (count($nums) == 2) return [$nums[0], $nums[1]];
-
-        asort($nums);
-
-        var_dump($nums);
-
-        for ($i = 0, $j = count($nums) -1; $i < count($nums) - 1; ($nums[$i] + $nums[$j] > $target ? $j-- : $i++)) {
-            if ($nums[$i] + $nums[$j] == $target) return array_keys([$nums[$i], $nums[$j]]);
+        $hashMap = [];
+        $count = count($nums);
+        for($i=0;$i<$count;$i++){
+                $secondIndex = $target-$nums[$i];
+                if(isset($hashMap[$secondIndex])){
+                    return array($i,$hashMap[$secondIndex]);
+                }else{
+                    $hashMap[$nums[$i]]=$i;
+                }
         }
+        return [];
     }
 }
-
-$solution = new Solution();
-print_r($solution->twoSum([3,2,4], 6)); // [0,1]
 
 
